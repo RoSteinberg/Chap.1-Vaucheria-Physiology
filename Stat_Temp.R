0bcccc863e7594bf30c95b13631d0d1c3dcee71b
RoSteinberg <rsteinbe@uni-bremen.de>
2023-01-09 17:42:52 +0100

    Statistics

---
+++ Stat_Temp.R
@@ -0,0 +1,9 @@
+# Sylt Temperature 2022 Statistics
+# Ronny Steinberg
+# 09.01.2023
+
+# Load librarys ---------------------------------------------------------------
+
+library(tidyverse)
+library(lubridate)
+library(ggpubr)

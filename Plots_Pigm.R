98ce71d2e96b9f1cd54c1447210c0105eea51712
RoSteinberg <rsteinbe@uni-bremen.de>
2023-01-16 12:14:07 +0100

    Pigment -Plots

--- Plots_Pigm.R
+++ Plots_Pigm.R
@@ -10,10 +10,32 @@ library(ggpubr)
 library(car)
 
 pigments <- 
-  read.delim2('C:/Users/rosteinb/Chap.1-Vaucheria-Physiology/data/Pigments.csv', header=TRUE, sep=",", dec=".") >%>
+  read.delim2('C:/Users/rosteinb/Chap.1-Vaucheria-Physiology/data/Pigments.csv', header=TRUE, sep=",", dec=".") %>%
   group_by(Site, Pigment) %>% 
-  # Then calculate the mean for each year and month
-  summarise(mean_day = mean(Temperature...C, na.rm = TRUE), 
+  # Then calculate the mean for each pigment and site
+  summarise(mean_conc = mean(Sample.Conc..µg., na.rm = TRUE), 
             # and the SD
-            sd_day = sd(Temperature...C, na.rm = TRUE),
+            sd_conc = sd(Sample.Conc..µg., na.rm = TRUE),
             .groups = 'drop')
+
+pigment_plot <- 
+  pigments %>% 
+  group_by(as.factor(Site), as.factor(Pigment)) %>% 
+  ggplot(aes(x = Pigment, y = mean_conc, fill = as.factor(Site))) + 
+  # Create dots
+  geom_bar(stat='identity',position = position_dodge(width = 1), color = 'black') +
+  geom_errorbar(aes(ymin = mean_conc-0, ymax = mean_conc + sd_conc), width = 0.2, position = position_dodge(width = 1), color = 'black') +
+  # Change labels
+  labs(x = " ", y = "Concentration (µg g)") +
+  theme_pubclean() +
+  scale_color_distiller(palette = 'Paired')
+pigment_plot
+
+read.delim2('C:/Users/rosteinb/Chap.1-Vaucheria-Physiology/data/Pigments.csv', header=TRUE, sep=",", dec=".") %>%
+  group_by(as.factor(Site), as.factor(Pigment)) %>% 
+  ggplot(aes(x = Pigment, y = Sample.Conc..µg.,color = as.factor(Site)))+
+  geom_boxplot()+
+  labs(x = '', y = 'Concentration (µg g)')+
+  theme_pubclean()+
+  scale_color_brewer(palette = 'Paired')
+ 

ad9b55b7b8f6eefa4c86a4a48ba39d837ca50567
RoSteinberg <rsteinbe@uni-bremen.de>
2023-01-17 22:05:23 +0100

    FvFm Data

---
+++ Plots_FvFm_Lab.R
@@ -0,0 +1,31 @@
+# Vaucheria FvFM_Lab2022 
+# Ronny Steinberg
+# 17.01.2023
+
+# Load librarys ---------------------------------------------------------------
+
+library(tidyverse)
+library(lubridate)
+library(ggpubr)
+library(car)
+
+FvFm_Lab <- 
+  read.delim2('C:/Users/rosteinb/Chap.1-Vaucheria-Physiology/data/FvFm_LabEx.csv', header=TRUE, sep=",", dec=".") %>%
+  filter(Day == '7') %>%
+  group_by(Temp) %>%
+  summarise(FvFm_mean = mean(FvFm, na.rm = TRUE), 
+            FvFm_sd = sd(FvFm, na.rm = TRUE),
+            .groups = 'drop')  
+
+read.delim2('C:/Users/rosteinb/Chap.1-Vaucheria-Physiology/data/FvFm_LabEx.csv', header=TRUE, sep=",", dec=".") %>%
+  filter(Day == '7') %>%
+  group_by(Temp) %>%
+  summarise(FvFm_mean = mean(FvFm, na.rm = TRUE), 
+            FvFm_sd = sd(FvFm, na.rm = TRUE),
+            .groups = 'drop') %>%
+  ggplot(aes(x = Temp, y = FvFm_mean, fill = Temp)) + 
+  geom_bar(stat='identity', color = 'black') +
+  geom_errorbar(aes(ymin = FvFm_mean-0, ymax = FvFm_mean + FvFm_sd), width = 0.2, position = position_dodge(width = 1), color = 'black') +
+  labs(x = " ", y = "Fv/Fm") +
+  theme_pubclean() +
+  scale_fill_distiller(palette = 'Paired')

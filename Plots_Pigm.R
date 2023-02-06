d24c23cab7c6562259720387e65576a681237140
RoSteinberg <rsteinbe@uni-bremen.de>
2023-02-06 15:25:21 +0100

    Pigments

--- Plots_Pigm.R
+++ Plots_Pigm.R
@@ -10,8 +10,8 @@ library(ggpubr)
 library(car)
 
 pigments <- 
-  read.delim2('C:/Users/rosteinb/Chap.1-Vaucheria-Physiology/data/Pigments.csv', header=TRUE, sep=",", dec=".") %>%
-  group_by(Site, Pigment) %>% 
+  read.delim2('C:/Users/rosteinb/Chap.1-Vaucheria-Physiology/data/Pigments_2.csv', header=TRUE, sep=",", dec=".") %>%
+  group_by(Site, Month, Pigment) %>% 
   # Then calculate the mean for each pigment and site
   summarise(mean_conc = mean(Sample.Conc..µg., na.rm = TRUE), 
             # and the SD
@@ -20,11 +20,14 @@ pigments <-
 
 pigment_plot <- 
   pigments %>% 
-  group_by(as.factor(Site), as.factor(Pigment)) %>% 
-  ggplot(aes(x = Pigment, y = mean_conc, fill = as.factor(Site))) + 
+  group_by(as.factor(Site), as.factor(Month),as.factor(Pigment)) %>% 
+  ggplot(aes(x = Month, y = mean_conc, group = Site, fill = as.factor(Pigment))) + 
   # Create dots
-  geom_bar(stat='identity',position = position_dodge(width = 1), color = 'black') +
-  geom_errorbar(aes(ymin = mean_conc-0, ymax = mean_conc + sd_conc), width = 0.2, position = position_dodge(width = 1), color = 'black') +
+  geom_bar(stat='identity', position = position_dodge2(width = 1), color = 'black') +
+  geom_errorbar(aes(ymin = mean_conc-0, ymax = mean_conc + sd_conc), width = 0.9, position = position_dodge2(width = 1), color = 'black') +
+  scale_x_discrete(limits = c('July', 'September', 'November'),
+                   labels = c('July', 'September', 'November'),
+                   ) +
   # Change labels
   labs(x = " ", y = "Concentration (µg g)") +
   theme_pubclean() +
